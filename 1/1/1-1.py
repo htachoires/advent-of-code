1c521c9154cf2175a1567e821f1ba0c501d31ca5
Hugo Tachoires <hugo.tachoires@gmail.com>
2023-12-01 15:08:33 +0100

    1-1

--- 1/1/1-1.py
+++ 1/1/1-1.py
@@ -7,6 +7,6 @@ def only_first_last_digit_to_int(text):
     res = list(filter(str.isdigit, text))
     return int(''.join([res[0], res[-1]]))
 
-digit = functools.reduce(lambda a,b: a+b, [only_first_last_digit_to_int(text) for text in lines])
+sum = functools.reduce(lambda a, b: a + b, [only_first_last_digit_to_int(text) for text in lines])
 
-print("1-1 -> {res}".format(res = digit))
+print("1-1 -> {res}".format(res = sum))
